a27648720bf2dcfc014f85c3652b46288f8ddf1b
mateja <mateja.zvegler1@gmail.com>
2021-05-23 04:17:07 +0200

    Kinda designed Action component.

--- app/src/views/couple/Action.js
+++ app/src/views/couple/Action.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import styles from "../../../assets/style/theme.scss"
 import { getRandomPose } from "../../api/ApiHandler"
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
@@ -21,7 +21,7 @@ export default function Action() {
         randomPose.then((resp) => {
             if (resp.status === 200) {
                 console.log(resp.data[0].title);
-                setPosition(resp.data);
+                setPosition(resp.data[0]);
             }
             else {
                 console.log("Can't get random pose.", resp.status, resp.data)
@@ -42,11 +42,26 @@ export default function Action() {
         <GestureRecognizer onSwipeLeft={(state) => onSwipeLeft()} config={config} style={styles.background} onTouchEnd={(state) => { receiveRandomPose(state) }}>
 
             <View>
-                <Text>{position[0].title}</Text>
-                <Text>{swipe}</Text>
-                <Text>Tap to change position, swipe left to finish.</Text>
-                <Text>Ne vem</Text>
+                <View style={styles.titleSection}>
+
+                    <Text style={styles.title}>{position.title}</Text>
+                    <Text style={styles.description}>{position.description}</Text>
+                </View>
+                <View style={styles.image}>
+                    {position.picture !== "smth" &&
+                        <Image style={ImageStyle.image} source={{ uri: `https://dhevkaliptus.herokuapp.com${position.picture}`, }} />
+                    }
+                </View>
+                <Text style={styles.instructions}>Tap to change position, slide left to finish.</Text>
             </View>
         </GestureRecognizer>
     )
 }
+
+const ImageStyle = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 350,
+        resizeMode: "contain"
+    }
+})
